4f513db54022e73de03faa2eb576102eb9b3ec8f
Varunesh Pathak <varuneshpathak21@gmail.com>
2025-02-12 21:24:40 +0530

    Fix error

--- url_shortner_api/server.js
+++ url_shortner_api/server.js
@@ -38,4 +38,4 @@ swaggerDocs(app);
 
 // Start server
 const PORT = process.env.PORT || 8000;
-app.listen(PORT,"localhost", () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT,'0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));
